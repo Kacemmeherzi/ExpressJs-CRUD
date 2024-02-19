18ae39473968dbd92710df3a2b03f6d44b754503
kacem <kacemmeherzi52@gmail.com>
2024-02-19 11:56:54 +0100

    CRUD Copleted

--- routes/voitures.js
+++ routes/voitures.js
@@ -1,7 +1,12 @@
+const e = require("express");
 const express = require ("express")
 const router = express.Router()
 const voitures = [{id:1,name:"clio"},{id:2,name:"megane"},{id:3,name:"range"}]
-
+router.get('/', (req, res) => {
+  
+ 
+   res.json(voitures) ;
+});
   router.get('/voi/:id', (req, res) => {
     const id = parseInt(req.params.id);
     console.log (id ) 
@@ -14,11 +19,23 @@ router.delete('/remove/:id', (req, res) => {
     
     let voitures = array.filter(item => item.id !== id);
      res.json(voitures) ;
-  });
+  }); 
   
-  router.get('/update/:id', (req, res) => {
+  router.put('/update/:id', (req, res) => {
+    const id1 = parseInt(req.params.id);
+     const updatedItem = req.body;
+     if (voitures.find(e => e.id===id1)!=null) {
+      
+     var  voiture = voitures.find (e =>e.id === id1 ) ;
+     voiture.name = req.body.name ; 
+      res.json("UPDATE DONE ") ;
+    
 
-  });
+  } else {
+    res.json("Erreur") ;
+  }
+
+});
 
 
 
